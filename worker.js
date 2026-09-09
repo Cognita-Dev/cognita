6c15e74201ec79b1723e39dbedb932542638c31a
gbemigaakinde <gbemigaakinde@gmail.com>
2026-09-10 00:23:21 +0100

    Update worker.js

--- worker.js
+++ worker.js
@@ -7,10 +7,12 @@ import { handlePlansRequest } from './plans-endpoint.js';
 import { handleChatRequest } from './chat-endpoint.js';
 import { handleImageRequest } from './image-endpoint.js';
 import { handleDocumentRequest } from './document-endpoint.js';
+import { handleResourceGenerate, handleResourceDownload } from './resources-endpoint.js';
 import { handlePaymentInitialize } from './payment-endpoint.js';
 import { handlePaystackWebhook } from './webhook-endpoint.js';
 import { handleAccountRequest, handleUsageRequest } from './account-endpoint.js';
 import { handleSubscriptionCancel } from './cancel-endpoint.js';
+
 
 function _corsPreflight() {
   return new Response(null, {
@@ -56,6 +58,15 @@ export default {
 
     if (request.method === 'POST' && url.pathname === '/api/document') {
       return handleDocumentRequest(request, env);
+    }
+
+    if (request.method === 'POST' && url.pathname === '/api/resources/generate') {
+      return handleResourceGenerate(request, env);
+    }
+
+    if (request.method === 'POST' && /^\/api\/resources\/[^/]+\/download$/.test(url.pathname)) {
+      const resourceId = url.pathname.split('/')[3];
+      return handleResourceDownload(request, env, resourceId);
     }
 
     if (request.method === 'POST' && url.pathname === '/api/payment/initialize') {
