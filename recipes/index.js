a78aad45d549e4170613554b24b3d90179a483f5
gbemigaakinde <gbemigaakinde@gmail.com>
2026-09-10 00:48:14 +0100

    Add new recipes and labels for exam and quiz

--- recipes/index.js
+++ recipes/index.js
@@ -5,12 +5,31 @@
 
 import { LESSON_PLAN_RECIPE } from './lesson-plan.js';
 import { WORKSHEET_RECIPE } from './worksheet.js';
+import { EXAM_RECIPE } from './exam.js';
+import { SCHEME_OF_WORK_RECIPE } from './scheme-of-work.js';
+import { QUIZ_RECIPE } from './quiz.js';
+import { STUDY_GUIDE_RECIPE } from './study-guide.js';
 
 export const RECIPES = {
   lesson_plan: LESSON_PLAN_RECIPE,
   worksheet: WORKSHEET_RECIPE,
+  exam: EXAM_RECIPE,
+  scheme_of_work: SCHEME_OF_WORK_RECIPE,
+  quiz: QUIZ_RECIPE,
+  study_guide: STUDY_GUIDE_RECIPE,
 };
 
 export function getRecipe(resourceType) {
   return RECIPES[resourceType] || null;
 }
+
+// Used by the frontend to build the "choose a resource type" list without
+// hard-coding labels in resources.js.
+export const RECIPE_LABELS = {
+  lesson_plan: 'Lesson Plan',
+  worksheet: 'Worksheet',
+  exam: 'Examination',
+  scheme_of_work: 'Scheme of Work',
+  quiz: 'Quiz',
+  study_guide: 'Study Guide',
+};
